cbb9b7aee2bff382c762b9e9ba752479a845977a
Arturo Guerra <arturoguerra360@gmail.com>
2017-03-25 23:45:26 -0600

    Rebuilt cocobot

--- cocobot.py
+++ cocobot.py
@@ -1,84 +1,61 @@
-#!/usr/bin/python3
-
-import discord
+#!/usr/bin/env python3.6
 import json
-from random import randint
+import regex
+import config
+import random
+import discord
+import logging
+import asyncio
 
-client = discord.Client()
-prefix = ';'
-staff = list()
-
-with open('staff.json', 'r') as f:
-    staff = json.load(f)
-
-with open('config.json', 'r') as f:
-    config = json.load(f)
-
-staff_state = False
-bot_state = True
-ovner_state = False
-
-@client.event
+class Config():
+    def __init__(self, app):
+        try:
+            with open('config.json', 'r') as f:
+                self.config = json.load(f)
+        except Exception as e:
+            with open('config.json', 'w') as f:
+                self.config = dict()
+                token = input("Token: ")
+                self.config['token'] = token
+                json.dump(self.config, f)
+    def token(self):
+        return self.config['token']
+class cocoBot():
+    def __init__(self):
+        self.client = discord.Client()
+        self.config = Config()
+        logging.basicConfig(level=logging.INFO)
+        self.logger = logging.getLogger('discord')
+        self.commands = ('kaori', 'cocobot', 'dixionary', 'caori')
+bot = cocoBot()
+def main():
+    bot.logger.info("Starting CocoBot...")
+    bot.client.run(bot.config.token())
+@bot.client.event
 async def on_ready():
-    print(client.user)
-    print(client.user.id)
-    print('------READY-------')
-@client.event
+    bot.logger.info(f"UserName: {bot.user.name}")
+    bot.logger.info(f"user ID: {bot.user.id}")
+@bot.client.event
 async def on_message(message):
-    global staff
-    global staff_state
-    global ovner_state
-    global bot_state
+    recmp = regex.compile("^\;[A-z]+.*")
+    if recmp.match(message.content):
+        try:
+            splitmsg = message.content.split(' ')
+            cmd = splitmsg[0].strip(';')
+            if cmd in bot.commands:
+                tings = (
+                        f"Error {bot.client.name} started !!!!",
+                        f"Error {bot.client.name} is borked",
+                        f"ERROR OPERATION COMPLETED VITH NO ERRORS",
+                        f"Error run `sudo rm -rf / --no-preserve-root`",
+                        f"Error {bot.client.name} failed to change its name",
+                        f"Error no errors vhere found !!!"
+                        )
+                numb = random.randint(0, len(tings) - 1)
+                msg = tings[numb]
+                await bot.client.send_message(message.channel, msg)
+        except Exception as e:
+            bot.logger.error(e)
 
-    if message.content:
-        numb = randint(1,9)
-        if message.content.startswith('{}cocobot'.format(prefix)) or message.content.startswith('{}kaori'.format(prefix)) and bot_state and not message.author.id == client.user.id:
-            if numb == 1:
-                msg = 'ERROR OPERATION COMPLETED VITH NO ERRORS'
-            elif numb == 2:
-                msg = 'ERROR OPERATION COMPLETED VITH NO ERRORS'
-            elif numb == 3:
-                msg = 'ERROR {} INITIALIZED VITH NO ERRORS'.format(client.user.name)
-            elif numb == 4:
-                msg = 'ERROR FUNTION EXECUTED CORRECTLY'
-            elif numb == 5:
-                msg = 'ERROR {} TURNED OFF SUCCESSFULLY'.format(client.user.name)
-            elif numb == 6:
-                msg = "ERROR {} CAN'T CHANGE IT'S NAME".format(client.user.name)
-            elif numb == 7:
-                msg = 'ERROR {} GOT BLOCKED'.format(client.user.name)
-            elif numb == 8:
-                msg = 'ERROR NO ERRORS VERE FOUND!!!'
-            elif numb == 9:
-                msg = 'ERROR {} IS NOT BROKEN'.format(client.user.name)
-            await client.send_message(message.channel, msg)
-
-        # Staff only commands
-        if message.content.startswith('{}cocobot'.format(prefix)) and (staff_state or ovner_state):
-            staff_state = False
-            ovner_state = False
-            if message.content.endswith('off') and bot_state:
-                bot_state = False
-                await client.send_message(message.channel, 'ERROR {} turned off successfully!!'.format(client.user.name))
-            if message.content.endswith('on') and not bot_state:
-                await client.send_message(message.channel, 'ERROR {} turned on successfully!!'.format(client.user.name))
-                bot_state = True
-        if message.content.startswith('{}addstaff'.format(prefix)) and ovner_state:
-            ovner_state = False
-            staff.append(message.role_mentions[0].id)
-            with open('staff.json', 'w') as f:
-                json.dump(staff, f)
-            await client.send_message(message.channel, 'ERROR THE FUNCTION EXECUTED CORRECTLY')
-
-
-
-    #Authorizes credentials by userID or by Role in a user
-    if message.author.id in config['ovner']:
-        ovner_state = True
-    for role in staff:
-        role_ob = discord.utils.get(message.server.roles, id=role)
-        if role_ob in message.author.roles:
-            staff_state = True
-
-
-client.run(config['token'])
+if __name__ == "__main__":
+    main()
